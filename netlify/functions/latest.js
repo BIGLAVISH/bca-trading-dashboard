3b581feb313c32d247776e421c291a1a752b9a24
BIGLAVISH <bca.enterprises22@gmail.com>
2026-05-01 02:53:48 -0400

    Update latest.js

--- netlify/functions/latest.js
+++ netlify/functions/latest.js
@@ -1,7 +1,18 @@
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
-    body: JSON.stringify([])
-  };
+import { getStore } from "@netlify/blobs";
+
+export default async () => {
+  try {
+    const store = getStore("signals");
+
+    const data = await store.get("data", { type: "json" }) || [];
+
+    return new Response(JSON.stringify(data), {
+      headers: { "Content-Type": "application/json" }
+    });
+
+  } catch (err) {
+    return new Response(JSON.stringify({ error: err.message }), {
+      status: 500
+    });
+  }
 };
