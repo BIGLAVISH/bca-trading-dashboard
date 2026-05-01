e0560f108675cf7b8306f12043e6880bb48b9210
BIGLAVISH <bca.enterprises22@gmail.com>
2026-05-01 01:24:18 -0400

    Update latest.js

--- netlify/functions/latest.js
+++ netlify/functions/latest.js
@@ -1,7 +1,23 @@
+const { getStore } = require("@netlify/blobs");
+
 exports.handler = async () => {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(global.signals || [])
-  };
+  try {
+    const store = getStore("bca-signals");
+    const signals = JSON.parse((await store.get("signals")) || "[]");
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify(signals)
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ ok: false, error: e.message })
+    };
+  }
 };
